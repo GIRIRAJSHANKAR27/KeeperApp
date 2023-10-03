ab237d4434a205ccf94b3714767413139abab0f1
GIRIRAJSHANKAR27 <118902211+GIRIRAJSHANKAR27@users.noreply.github.com>
2023-10-03 23:43:01 +0530

    inital commit

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Notes from "./note";
+
+function App(){
+    return <div>
+        <Header />
+        <Notes />
+        <Footer />
+    </div>
+}
+ export default App;    
---
+++ src/components/note.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+
+function Note(){
+    return (
+        <div className="note">
+            <h1>This is the heading.</h1>
+            <p>This is the content.</p>
+        </div>
+    );
+}
+
+export default Note;
